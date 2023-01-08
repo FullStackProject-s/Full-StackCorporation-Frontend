7d658ed5bb2b525262dcb8418c89d944358c0345
GregDemidenkov <greg.demidenkov@yandex.ru>
2023-01-09 00:37:55 +0300

    Developed my organizations page
    Developed my organizations page. Developed component for content of my organization page, organization cart, organization button.

---
+++ src/components/pages/myOrganizations/myOrganizationsContent/MyOrganizationsContent.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+import { OrganizationCart } from './organizationCart/OrganizationCart'
+
+import styles from './myOrganizationsContent.module.scss'
+
+export const MyOrganizationsContent: React.FC = () => {
+
+    return (
+        <div className = {styles.myOrganizationsContent}>
+            <div className = {styles.createOrgWrapper}>
+                <NavLink to = "">Create Org</NavLink>
+            </div>
+            <h3 className = {styles.sectionName}>My Organizations</h3>
+            <div className = {styles.orgList}>
+                <OrganizationCart orgClass = "myOrg"/>
+                <OrganizationCart orgClass = "myOrg"/>
+            </div>
+            <h3 className = {styles.sectionName}>Organizations</h3>
+            <div className = {styles.orgList}>
+                <OrganizationCart orgClass = "org"/>
+                <OrganizationCart orgClass = "org"/>
+                <OrganizationCart orgClass = "org"/>
+            </div>
+        </div>
+    )
+}
