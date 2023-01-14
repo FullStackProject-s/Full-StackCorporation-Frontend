b1f99c311370393cea3964b2b01fc706a437a028
Bol4onok <95026332+MihailGulkin@users.noreply.github.com>
2023-01-14 15:30:40 +0300

    first setup

---
+++ src/pages/login.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+export const Login = () =>
+{
+    return (
+         <div className="justify-center flex">
+      <div className="w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col  w-full mt-10">
+        <h2 className="text-gray-900 text-lg font-medium title-font mb-5">
+          Sign Up
+        </h2>
+        <div className="relative mb-4">
+          <label
+            htmlFor="full-name"
+            className="leading-7 text-sm text-gray-600"
+          >
+            Full Name
+          </label>
+          <input
+            type="text"
+            id="full-name"
+            name="full-name"
+            className="w-full bg-white rounded border border-gray-300
+                                   focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200
+                                    text-base outline-none text-gray-700 py-1 px-3 leading-8
+                                     transition-colors duration-200 ease-in-out"
+          />
+        </div>
+        <div className="relative mb-4">
+          <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          />
+        </div>
+        <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+          Button
+        </button>
+        <p className="text-xs text-gray-500 mt-3">
+          Literally you probably haven't heard of them jean shorts.
+        </p>
+      </div>
+    </div>
+    )
+};
