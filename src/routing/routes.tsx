9f0f9de1851951847128ff2ada5b9c388e023468
GregDemidenkov <greg.demidenkov@yandex.ru>
2023-01-27 00:11:43 +0300

    Add HomePage component and route

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -4,15 +4,18 @@ import { LoginPage } from "pages/auth/formPage/LoginPage";
 import { RegistrationPage } from "pages/auth/formPage/RegistrationPage";
 import { ChangePasswordPage } from "pages/auth/formPage/ChangePasswordPage";
 
+import { HomePage } from "pages/homePage/HomePage";
 import { OrganizationsPage } from "pages/organizations/OrganizationsPage";
 import { ProfilePage } from "pages/profile/ProfilePage";
 import { MyOrganizations } from "pages/myOrganizations/MyOrganizations";
 
 export const RoutesList = [
+    {path: paths.homePage, element: <HomePage/>},
     {path: paths.organizationsPage, element: <OrganizationsPage/>},
     {path: paths.loginPage, element: <LoginPage/>},
     {path: paths.registrationPage, element: <RegistrationPage/>},
     {path: paths.changePasswordPage, element: <ChangePasswordPage/>},
     {path: paths.profilePage, element: <ProfilePage/>},
     {path: paths.myOrganizations, element: <MyOrganizations/>},
+    {path: paths.missing, element: <HomePage/>},
 ]
