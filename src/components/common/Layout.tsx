39f7d81b40c315d0b4870963fe6164746d360c38
GregDemidenkov <greg.demidenkov@yandex.ru>
2023-02-13 22:15:21 +0300

    Fix scss

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -20,7 +20,9 @@ export const Layout: React.FC = () => {
   return(
     <>
       <Header />
-      <Outlet/>
+      <div className='outlet'>
+        <Outlet/>
+      </div>
       <Footer/>
     </>
   )
